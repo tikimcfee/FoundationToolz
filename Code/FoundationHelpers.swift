21c3c475feaf37119d1f17552cdfbf84ece9db6f
Sebastian <contact@flowtoolz.com>
2018-11-26 15:48:13 +0100

    Add default predicate "all"

--- Code/FoundationHelpers.swift
+++ Code/FoundationHelpers.swift
@@ -33,3 +33,8 @@ public var appName: String? =
     
     return Bundle.main.infoDictionary?[key] as? String
 }()
+
+extension NSPredicate
+{
+    public static var all: NSPredicate { return NSPredicate(value: true) }
+}
