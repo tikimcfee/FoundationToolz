ac7c6df63f8fe61c3c19463ad661f651992471c6
Sebastian <contact@flowtoolz.com>
2018-11-30 13:09:34 +0100

    Reimplement NetworkReachability, add manual observation mechanism

--- Code/NetworkReachability.swift
+++ Code/NetworkReachability.swift
@@ -1,52 +1,62 @@
 import Foundation
-
-public let networkReachability = NetworkReachability()
+import Reachability
 
 public class NetworkReachability
 {
-    fileprivate init() {}
+    public static let shared = NetworkReachability()
     
-    public var isReachable = true
+    // MARK: - Initialization
     
-    public func setup()
+    private init()
     {
-        guard let reachability = reachabilityObject else
+        guard let reachability = reachability else
         {
-            print("Creating Reachability Object failed")
+            print("ERROR: Reachability object couldn't be created.")
             return
         }
         
-        reachability.whenReachable =
-        {
-            reachability in
-            
-            DispatchQueue.main.async
-            {
-                self.isReachable = true
-            }
-        }
-        
-        reachability.whenUnreachable =
-        {
-            reachability in
-            
-            DispatchQueue.main.async
-            {
-                self.isReachable = false
-            }
-        }
+        reachability.whenReachable = update
+        reachability.whenUnreachable = update
         
         do
         {
             try reachability.startNotifier()
         }
-        catch
+        catch let error
         {
-            print("Starting Reachability Notifier failed")
+            print("ERROR: \(error.localizedDescription)")
         }
-        
-        isReachable = reachability.isReachable
     }
     
-    private let reachabilityObject = Reachability()
+    // MARK: - Observing
+    
+    // TODO: Consider adding SwiftObserver as dependency to FoundationToolz for observing network reachability
+    private func update(reachability: Reachability)
+    {
+        observers.removeAll { $0.observer == nil }
+        observers.forEach { $0.notify(reachability.connection) }
+    }
+    
+    public func notifyOfChanges(_ observer: AnyObject,
+                                action: @escaping (Reachability.Connection) -> Void)
+    {
+        observers.append(WeakObserver(observer: observer, notify: action))
+    }
+    
+    private var observers = [WeakObserver]()
+    
+    private struct WeakObserver
+    {
+        weak var observer: AnyObject?
+        let notify: (Reachability.Connection) -> Void
+    }
+    
+    // MARK: - Basics
+    
+    public var connection: Reachability.Connection?
+    {
+        return reachability?.connection
+    }
+    
+    private let reachability = Reachability()
 }
