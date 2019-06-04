22534d3f5fbf7cad9e51327b128a806ca306f0ab
flowtoolz <contact@flowtoolz.com>
2019-06-05 01:14:26 +0200

    Extract func to get uptime in nano seconds

--- Code/Double+Uptime.swift
+++ Code/Double+Uptime.swift
@@ -4,14 +4,14 @@ public extension Double
 {
     static var uptimeMilliseconds: Double
     {
+        return Double(uptimeNanoSeconds) / 1000000.0
+    }
+    
+    static var uptimeNanoSeconds: UInt64
+    {
         let ticks = mach_absolute_time()
-        
         var timeBase = mach_timebase_info_data_t()
-        
         mach_timebase_info(&timeBase)
-        
-        let nanoSeconds = ticks * UInt64(timeBase.numer) / UInt64(timeBase.denom)
-        
-        return Double(nanoSeconds) / 1000000.0
+        return ticks * UInt64(timeBase.numer) / UInt64(timeBase.denom)
     }
 }
