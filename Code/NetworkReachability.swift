23882522d69415f3a73b944fae48e6e1fc3a97d8
flowtoolz <contact@flowtoolz.com>
2019-07-27 13:30:12 +0200

    Adjust NetworkReachability API

--- Code/NetworkReachability.swift
+++ Code/NetworkReachability.swift
@@ -19,7 +19,7 @@ public class NetworkReachability
         }
         else
         {
-            initialzeWithReachabilityPod()
+            initialzeWithReachabilityCocoaod()
         }
     }
     
@@ -28,18 +28,18 @@ public class NetworkReachability
     @available(OSX 10.14, iOS 12.0, tvOS 12.0, *)
     private func notifyObserversWithNetworkPath(_ networkPath: NWPath)
     {
-        let connectivity: Update =
+        let update: Update =
         {
             guard networkPath.status == .satisfied else { return .noInternet }
             return networkPath.isExpensive ? .expensiveInternet : .fullInternet
         }()
         
-        notifyObservers(of: connectivity)
+        sendToObservers(update)
     }
     
     // MARK: - Based On Reachability Cocoapod
     
-    private func initialzeWithReachabilityPod()
+    private func initialzeWithReachabilityCocoaod()
     {
         guard let reachability = reachability else
         {
@@ -47,8 +47,8 @@ public class NetworkReachability
             return
         }
         
-        reachability.whenReachable = notifyObserversWithReachability
-        reachability.whenUnreachable = notifyObserversWithReachability
+        reachability.whenReachable = sendToObservers
+        reachability.whenUnreachable = sendToObservers
         
         do
         {
@@ -56,13 +56,13 @@ public class NetworkReachability
         }
         catch
         {
-            log(error: error.localizedDescription)
+            log(error: error.readable.message)
         }
     }
     
-    private func notifyObserversWithReachability(_ reachability: Reachability)
+    private func sendToObservers(_ reachability: Reachability)
     {
-        let connectivity: Update =
+        let update: Update =
         {
             switch reachability.connection
             {
@@ -72,7 +72,7 @@ public class NetworkReachability
             }
         }()
         
-        notifyObservers(of: connectivity)
+        sendToObservers(update)
     }
     
     public var connection: Reachability.Connection?
@@ -84,22 +84,21 @@ public class NetworkReachability
     
     // MARK: - Primitive Observability
     
-    public func notifyOfChanges(_ observer: AnyObject,
-                                action: @escaping (Update) -> Void)
+    public func add(observer: AnyObject, receive: @escaping (Update) -> Void)
     {
-        observers.append(WeakObserver(observer: observer, notify: action))
+        observers.append(WeakObserver(observer: observer, receive: receive))
     }
     
-    public func stopNotifying(_ observer: AnyObject)
+    public func remove(observer: AnyObject)
     {
         // TODO: use SwiftyToolz to properly hash observers
         observers.removeAll { $0.observer === observer }
     }
     
-    private func notifyObservers(of connectivity: Update)
+    private func sendToObservers(_ update: Update)
     {
         observers.removeAll { $0.observer == nil }
-        observers.forEach { $0.notify(connectivity) }
+        observers.forEach { $0.receive(update) }
     }
     
     private var observers = [WeakObserver]()
@@ -107,7 +106,7 @@ public class NetworkReachability
     private struct WeakObserver
     {
         weak var observer: AnyObject?
-        let notify: (Update) -> Void
+        let receive: (Update) -> Void
     }
     
     public enum Update { case noInternet, expensiveInternet, fullInternet }
