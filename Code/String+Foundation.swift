4a728b1b0beb6ce6d49deca3889de9ee57e82fe3
flowtoolz <contact@flowtoolz.com>
2019-11-29 00:12:31 +0100

    Add fileExtension: String

--- Code/String+Foundation.swift
+++ Code/String+Foundation.swift
@@ -21,6 +21,15 @@ public extension String
         URL(fileURLWithPath: self).lastPathComponent
     }
     
+    var fileExtension: String?
+    {
+        let parts = components(separatedBy: ".")
+        guard let lastPart = parts.last,
+            parts.count > 1,
+            (1 ... 4).contains(lastPart.count) else { return nil }
+        return lastPart
+    }
+    
     func dateString(fromFormat: String, toFormat: String) -> String
     {
         guard let date = Date(fromString: self, withFormat: fromFormat) else
