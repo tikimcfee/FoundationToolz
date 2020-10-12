1622bd3367778753e1c3c809f25cea98523576ce
flowtoolz <contact@flowtoolz.com>
2020-10-12 12:40:13 +0200

    Evolve HTTP Request helpers

---
+++ Code/URL+HTTPRequests.swift
@@ -0,0 +1,115 @@
+import Foundation
+import SwiftyToolz
+
+public extension URL
+{
+    func get<Value: Decodable>(_ type: Value.Type,
+                               handleResult: @escaping (Result<Value, RequestError>) -> Void)
+    {
+        URLSession.shared.dataTask(with: self)
+        {
+            data, response, error in
+            
+            if let error = error
+            {
+                let nsError = error as NSError
+                let isURLError = nsError.domain == URLError.errorDomain
+                let urlErrorCode = isURLError ? URLError.Code(rawValue: nsError.code) : nil
+                return handleResult(.failure(.receivingResponseFailed(nsError, urlErrorCode)))
+            }
+            
+            let httpResponse = response as! HTTPURLResponse
+            
+            guard (200 ... 299).contains(httpResponse.statusCode) else
+            {
+                return handleResult(.failure(.validatingResponseStatusFailed(httpResponse, data)))
+            }
+                
+            guard let data = data else
+            {
+                return handleResult(.failure(.receivingDataFailed(httpResponse)))
+            }
+            
+            guard let value = Value(fromJSON: data) else
+            {
+                return handleResult(.failure(.decodingDataFailed(httpResponse, data)))
+            }
+            
+            handleResult(.success(value))
+        }
+        .resume()
+    }
+    
+    func post<Value: Encodable>(_ value: Value,
+                                handleError: @escaping (RequestError?) -> Void)
+    {
+        guard let valueData = value.encode() else
+        {
+            return handleError(.encodingDataFailed)
+        }
+        
+        var request = URLRequest(url: self)
+        request.httpBody = valueData
+        
+        URLSession.shared.dataTask(with: request)
+        {
+            data, response, error in
+            
+            if let error = error
+            {
+                let nsError = error as NSError
+                let isURLError = nsError.domain == URLError.errorDomain
+                let urlErrorCode = isURLError ? URLError.Code(rawValue: nsError.code) : nil
+                return handleError(.receivingResponseFailed(nsError, urlErrorCode))
+            }
+            
+            let httpResponse = response as! HTTPURLResponse
+            
+            guard (200...299).contains(httpResponse.statusCode) else
+            {
+                return handleError(.validatingResponseStatusFailed(httpResponse, data))
+            }
+            
+            handleError(nil)
+        }
+        .resume()
+    }
+    
+    enum RequestError: Error, CustomStringConvertible, CustomDebugStringConvertible
+    {
+        public var localizedDescription: String { description }
+        
+        public var debugDescription: String { description }
+        
+        public var description: String
+        {
+            switch self
+            {
+            case .encodingDataFailed:
+                return "Could not endecode the data."
+            case .receivingResponseFailed(let nsError, let urlErrorCode):
+                var message = nsError.localizedDescription
+                if let urlErrorCode = urlErrorCode
+                {
+                    message += " URL error code: \(urlErrorCode.rawValue)"
+                }
+                return message
+            case .receivingDataFailed(let response):
+                let status = HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
+                return "Response has no data. HTTP Status: " + status
+            case .decodingDataFailed(let response, _):
+                let status = HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
+                return "Could not decode the data. HTTP Status: " + status
+            case .validatingResponseStatusFailed(let response, _):
+                let status = HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
+                return "Unexpected HTTP Status: " + status
+            }
+        }
+        
+        case encodingDataFailed
+        case receivingResponseFailed(NSError, URLError.Code?)
+        case validatingResponseStatusFailed(HTTPURLResponse, Data?)
+        case receivingDataFailed(HTTPURLResponse)
+        case decodingDataFailed(HTTPURLResponse, Data)
+    }
+}
