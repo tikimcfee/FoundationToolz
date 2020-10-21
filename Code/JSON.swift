18837f26e26be965ab7f4c3b569ab09f4238f398
flowtoolz <contact@flowtoolz.com>
2020-10-21 12:41:06 +0200

    Adjust and clean up JSON implementation

--- Code/JSON.swift
+++ Code/JSON.swift
@@ -1,11 +1,20 @@
 import Foundation
 import SwiftyToolz
 
-@dynamicMemberLookup
-public enum JSON
+/// String Representation
+extension JSON: CustomStringConvertible, CustomDebugStringConvertible
 {
-    // MARK: - Conversions
+    public var debugDescription: String { description }
     
+    public var description: String
+    {
+        (try? data())?.utf8String ?? "Error creating description for \(self)"
+    }
+}
+
+/// Data Conversion
+extension JSON
+{
     public init(_ data: Data) throws
     {
         self = try Self(JSONSerialization.jsonObject(with: data))
@@ -13,10 +22,15 @@ public enum JSON
     
     public func data() throws -> Data
     {
-        try JSONSerialization.data(withJSONObject: jsonObject(), options: .prettyPrinted)
+        try JSONSerialization.data(withJSONObject: jsonObject(),
+                                   options: .prettyPrinted)
     }
-    
-    public init(_ jsonObject: Object) throws
+}
+
+/// JSON Object Conversion
+extension JSON
+{
+    public init(_ jsonObject: JSONObject) throws
     {
         switch jsonObject
         {
@@ -28,16 +42,16 @@ public enum JSON
             self = .int(int)
         case let string as String:
             self = .string(string)
-        case let array as ObjectArray:
+        case let array as JSONObjectArray:
             self = try .array(array.map(Self.init))
-        case let dictionary as ObjectDictionary:
+        case let dictionary as JSONObjectDictionary:
             self = try .dictionary(dictionary.mapValues(Self.init))
         default:
-            throw "Invalid JSON object"
+            throw "Invalid JSON object: \(jsonObject)"
         }
     }
     
-    private func jsonObject() -> Object
+    private func jsonObject() -> JSONObject
     {
         switch self
         {
@@ -55,52 +69,65 @@ public enum JSON
             return dictionary.mapValues { $0.jsonObject() }
         }
     }
-    
-    public typealias ObjectDictionary = [String: Object]
-    public typealias ObjectArray = [Object]
-    public typealias Object = Any
-    
-    // MARK: - Values
+}
+
+public typealias JSONObjectDictionary = [String: JSONObject]
+public typealias JSONObjectArray = [JSONObject]
+public typealias JSONObject = Any
+
+/// Case Access
+extension JSON
+{
+    // MARK: - Case Values
     
     func null(_ field: String? = nil) throws -> NSNull
     {
-        if let field = field { return try self.field(field).null() }
+        if let field = field { return try at(field).null() }
         guard case .null = self else { throw "JSON is not null" }
         return NSNull()
     }
     
     func bool(_ field: String? = nil) throws -> Bool
     {
-        if let field = field { return try self.field(field).bool() }
+        if let field = field { return try at(field).bool() }
         guard case .bool(let bool) = self else { throw "JSON is not a Bool" }
         return bool
     }
     
     func int(_ field: String? = nil) throws -> Int
     {
-        if let field = field { return try self.field(field).int() }
+        if let field = field { return try at(field).int() }
         guard case .int(let int) = self else { throw "JSON is not an Int" }
         return int
     }
     
     func string(_ field: String? = nil) throws -> String
     {
-        if let field = field { return try self.field(field).string() }
+        if let field = field { return try at(field).string() }
         guard case .string(let string) = self else { throw "JSON is not a String" }
         return string
     }
+    
+    func array(_ field: String? = nil) throws -> [JSON]
+    {
+        if let field = field { return try at(field).array() }
+        guard case .array(let array) = self else { throw "JSON is not an Array" }
+        return array
+    }
+    
+    func dictionary(_ field: String? = nil) throws -> [String: JSON]
+    {
+        if let field = field { return try at(field).dictionary() }
+        guard case .dictionary(let dict) = self else { throw "JSON is not a Dictionary" }
+        return dict
+    }
+    
+    // MARK: - Array Elements
     
     subscript(index: Int) -> JSON?
     {
         guard case .array(let array) = self else { return nil }
         return array.indices.contains(index) ? array[index] : nil
-    }
-    
-    func array(_ field: String? = nil) throws -> [JSON]
-    {
-        if let field = field { return try self.field(field).array() }
-        guard case .array(let array) = self else { throw "JSON is not an Array" }
-        return array
     }
     
     func at(_ index: Int) throws -> JSON
@@ -118,37 +145,44 @@ public enum JSON
         return array[index]
     }
     
+    // MARK: - Dictionary Elements
+    
     subscript(key: String) -> JSON?
     {
         guard case .dictionary(let dictionary) = self else { return nil }
         return dictionary[key]
     }
     
-    subscript(dynamicMember member: String) -> JSON?
-    {
-        guard case .dictionary(let dictionary) = self else { return nil }
-        return dictionary[member]
-    }
-    
-    func field(_ field: String) throws -> JSON
+    func at(_ key: String) throws -> JSON
     {
         guard case .dictionary(let dictionary) = self else
         {
             throw "JSON is not a dictionary"
         }
         
-        guard let json = dictionary[field] else
+        guard let json = dictionary[key] else
         {
-            throw "JSON dictionary contains no field \"\(field)\""
+            throw "JSON dictionary contains no field \"\(key)\""
         }
         
         return json
+    }
+}
+
+/// JSON with Dynamic Lookup of Dictionary Elements
+@dynamicMemberLookup
+public enum JSON
+{
+    subscript(dynamicMember member: String) -> JSON?
+    {
+        guard case .dictionary(let dictionary) = self else { return nil }
+        return dictionary[member]
     }
     
     case null
     case bool(Bool)
     case int(Int)
     case string(String)
-    case array(Array<JSON>)
-    case dictionary(Dictionary<String, JSON>)
+    case array([JSON])
+    case dictionary([String: JSON])
 }
