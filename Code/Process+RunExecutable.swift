c875cb495a64a5c3577d6832bee50a91f3a0c71e
flowtoolz <contact@flowtoolz.com>
2020-10-25 16:28:32 +0100

    Add Process extension for running console commands

---
+++ Code/Process+RunExecutable.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+extension Process
+{
+    @available(OSX 10.13, *)
+    func runExecutable(at filePath: String, arguments: [String]) throws -> String
+    {
+        let input = Pipe()
+        let output = Pipe()
+        
+        executableURL = URL(fileURLWithPath: filePath)
+        standardInput = input
+        standardOutput = output
+        environment = nil
+        self.arguments = arguments
+        
+        var outputData = Data()
+        
+        output.fileHandleForReading.readabilityHandler =
+        {
+            output in outputData += output.availableData
+        }
+        
+        try run()
+        waitUntilExit()
+        
+        return String(data: outputData, encoding: .utf8)!
+    }
+}
