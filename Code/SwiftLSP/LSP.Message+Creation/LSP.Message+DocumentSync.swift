43becb5d914a0d30db88277b51d5f8109fd43eb6
flowtoolz <contact@flowtoolz.com>
2020-10-29 03:33:20 +0100

    Extract SwiftLSP into dedicated package

--- Code/SwiftLSP/LSP.Message+Creation/LSP.Message+DocumentSync.swift
+++
@@ -1,9 +0,0 @@
-public extension LSP.Message.Notification
-{
-    // textDocument LSP type: TextDocumentItem
-    static func didOpen(doc: JSON) -> Self
-    {
-        .init(method: "textDocument/didOpen",
-              params: .dictionary(["textDocument": doc]))
-    }
-}
