78622e6c08b3cab238b8e35ec9b3f6efde724432
flowtoolz <contact@flowtoolz.com>
2020-10-29 04:11:06 +0100

    Fix access levels in JSON extensions

--- Code/JSON.swift
+++ Code/JSON.swift
@@ -37,9 +37,9 @@ extension JSON
 }
 
 /// JSON Object Conversion
-extension JSON
+public extension JSON
 {
-    public init(_ jsonObject: JSONObject) throws
+    init(_ jsonObject: JSONObject) throws
     {
         switch jsonObject
         {
@@ -85,42 +85,42 @@ extension JSON
 {
     // MARK: - Case Values
     
-    func null(_ field: String? = nil) throws -> NSNull
+    public func null(_ field: String? = nil) throws -> NSNull
     {
         if let field = field { return try at(field).null() }
         guard case .null = self else { throw "JSON is not null" }
         return NSNull()
     }
     
-    func bool(_ field: String? = nil) throws -> Bool
+    public func bool(_ field: String? = nil) throws -> Bool
     {
         if let field = field { return try at(field).bool() }
         guard case .bool(let bool) = self else { throw "JSON is not a Bool" }
         return bool
     }
     
-    func int(_ field: String? = nil) throws -> Int
+    public func int(_ field: String? = nil) throws -> Int
     {
         if let field = field { return try at(field).int() }
         guard case .int(let int) = self else { throw "JSON is not an Int" }
         return int
     }
     
-    func string(_ field: String? = nil) throws -> String
+    public func string(_ field: String? = nil) throws -> String
     {
         if let field = field { return try at(field).string() }
         guard case .string(let string) = self else { throw "JSON is not a String" }
         return string
     }
     
-    func array(_ field: String? = nil) throws -> [JSON]
+    public func array(_ field: String? = nil) throws -> [JSON]
     {
         if let field = field { return try at(field).array() }
         guard case .array(let array) = self else { throw "JSON is not an Array" }
         return array
     }
     
-    func dictionary(_ field: String? = nil) throws -> [String: JSON]
+    public func dictionary(_ field: String? = nil) throws -> [String: JSON]
     {
         if let field = field { return try at(field).dictionary() }
         guard case .dictionary(let dict) = self else { throw "JSON is not a Dictionary" }
@@ -129,13 +129,13 @@ extension JSON
     
     // MARK: - Array Elements
     
-    subscript(index: Int) -> JSON?
+    public subscript(index: Int) -> JSON?
     {
         guard case .array(let array) = self else { return nil }
         return array.indices.contains(index) ? array[index] : nil
     }
     
-    func at(_ index: Int) throws -> JSON
+    public func at(_ index: Int) throws -> JSON
     {
         guard case .array(let array) = self else
         {
@@ -152,13 +152,13 @@ extension JSON
     
     // MARK: - Dictionary Elements
     
-    subscript(key: String) -> JSON?
+    public subscript(key: String) -> JSON?
     {
         guard case .dictionary(let dictionary) = self else { return nil }
         return dictionary[key]
     }
     
-    func at(_ key: String) throws -> JSON
+    public func at(_ key: String) throws -> JSON
     {
         guard case .dictionary(let dictionary) = self else
         {
@@ -178,7 +178,7 @@ extension JSON
 @dynamicMemberLookup
 public enum JSON
 {
-    subscript(dynamicMember member: String) -> JSON?
+    public subscript(dynamicMember member: String) -> JSON?
     {
         guard case .dictionary(let dictionary) = self else { return nil }
         return dictionary[member]
